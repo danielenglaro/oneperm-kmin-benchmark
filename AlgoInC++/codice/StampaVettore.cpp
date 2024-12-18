ba88f70aa46d68213706e0a4a7b0fadeff3ae2d0
Daniele <daniele.englaro@gmail.com>
2024-12-18 17:26:58 +0100

    modifica stampa

--- AlgoInC++/codice/StampaVettore.cpp
+++ AlgoInC++/codice/StampaVettore.cpp
@@ -10,7 +10,7 @@ void StampaVettore::printVector(const std::vector<uint64_t>& vettore) {
 
 void StampaVettore::printVector(const std::vector<double>& vettore) {
         std::cout << "[ ";
-        for (size_t val : vettore) {
+        for (double val : vettore) {
             std::cout << val << " ";
         }
         std::cout << "]" << std::endl;
