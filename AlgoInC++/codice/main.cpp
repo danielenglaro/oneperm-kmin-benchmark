9f1feb49f0225ffce0b4a48aef6b97194e0ce6c5
Daniele <daniele.englaro@gmail.com>
2025-02-06 17:06:44 +0100

    modifica main con selezione TEST da esecuzione

--- AlgoInC++/codice/main.cpp
+++ AlgoInC++/codice/main.cpp
@@ -1,13 +1,25 @@
 #include <iostream>
 #include "Test.h"
 
-
-int main()
+int main(int argc, char *argv[])
 {
     // Inizio del conteggio del tempo
     auto start = std::chrono::high_resolution_clock::now();
 //----------------------------------------------CRONOMETRO----------------------------------------------
 
+    // Controllo se è stato fornito almeno un parametro
+    if (argc != 2) {
+        printf("\nErrore: manca parametro x");
+        printf("\nUso:");
+        printf("\n      0 per eseguire test sul tempo con k fissato");
+        printf("\n      1 per eseguire test sul tempo con n fissato");
+        printf("\n      2 per eseguire tutti i test sul tempo");
+        printf("\n      3 per eseguire test sulla qualità");
+        printf("\n      0 per eseguire tutti i test");
+        return 1;  // esco con errore
+    }
+    // Converto il parametro in intero
+    int x = atoi(argv[1]);
     
     // Imposta il modulo dell'universo come il primo numero primo maggiore o uguale a 2^30
     size_t m = 1073741824;
@@ -18,48 +30,60 @@ int main()
 
 
 
-    // ---- TEST per analizzare il tempo al variare di n ----
-    std::vector<int> n_range;
-    for (int i = (1 << 10); i <= (1 << 20); i *= 2) {
-        n_range.push_back(i); // n range || Ciclo per generare le potenze da 2^10 fino a 2^20
-    }
+    // ------------ TEST sul tempo con k fissato ------------
     std::vector<int> vettore_k_fissato = { 16,256,4096};// k fissato a salti di 2^4
-    for(int k_fissato : vettore_k_fissato){
-        Test::test_time_vs_n(k_fissato, n_range, rep, m);
-
-        // Esegui lo script Python per generare il grafico dei tempi in funzione di n
-        system( ("python3 graficoTempo.py n " + std::to_string(k_fissato)).c_str() );
-
-        // system( ("open grafico_tempo_k=" + std::to_string(k_fissato) + ".png").c_str() );
-    }
-
-
-
-
-    // ---- TEST per analizzare il tempo in funzione di k ----
-    std::vector<int> k_range;
-    for (int i = 1; i <= 8192; i *= 2) k_range.push_back(i); // k range || Ciclo per generare le potenze di 2 fino a 2^16
-
-    std::vector<int> vettore_n_fissato = {10000, 100000, 1000000 }; // n fissato potenze di 10 da 10^2
-    for(int n_fissato : vettore_n_fissato){
-        Test::test_time_vs_k(k_range, n_fissato, rep, m);
-
-        // // Esegui lo script Python per generare il grafico dei tempi in funzione di k
-        system( ("python3 graficoTempo.py k " + std::to_string(n_fissato)).c_str() );
-
-        // system( ("open grafico_tempo_n=" + std::to_string(n_fissato) + ".png").c_str() );
-    }
-
-
-    
-    // ---- TEST per analizzare la qualità ----
-    int n = 50000;
-    for(int k_fissato : vettore_k_fissato){
-            Test::test_quality(k_fissato, n, rep, m);
+    if (x == 0 || x == 2 || x == 4)
+    {
+        std::vector<int> n_range;
+        for (int i = (1 << 10); i <= (1 << 20); i *= 2) {
+            n_range.push_back(i); // n range || Ciclo per generare le potenze da 2^10 fino a 2^20
+        }
+        for(int k_fissato : vettore_k_fissato){
+            Test::test_time_vs_n(k_fissato, n_range, rep, m);
 
             // Esegui lo script Python per generare il grafico dei tempi in funzione di n
-            system(("python3 graficoQualita.py " + std::to_string(k_fissato)).c_str());
+            system( ("python3 graficoTempo.py n " + std::to_string(k_fissato)).c_str() );
+
+            // system( ("open grafico_tempo_k=" + std::to_string(k_fissato) + ".png").c_str() );
         }
+    }
+
+
+
+
+    // ------------ TEST sul tempo con n fissato ------------
+    std::vector<int> vettore_n_fissato = {10000, 100000, 1000000 }; // n fissato potenze di 10 da 10^2
+    if (x == 1 || x == 2 || x == 4)
+    {
+        std::vector<int> k_range;
+        for (int i = 1; i <= 8192; i *= 2) k_range.push_back(i); // k range || Ciclo per generare le potenze di 2 fino a 2^16
+
+        for(int n_fissato : vettore_n_fissato){
+            Test::test_time_vs_k(k_range, n_fissato, rep, m);
+
+            // // Esegui lo script Python per generare il grafico dei tempi in funzione di k
+            system( ("python3 graficoTempo.py k " + std::to_string(n_fissato)).c_str() );
+
+            // system( ("open grafico_tempo_n=" + std::to_string(n_fissato) + ".png").c_str() );
+        }
+    }
+
+
+
+
+    // ------------ TEST per analizzare la qualità ------------
+    if (x == 3 || x == 4)
+    {
+        int n = 50000;
+        for(int k_fissato : vettore_k_fissato){
+                Test::test_quality(k_fissato, n, rep, m);
+
+                // Esegui lo script Python per generare il grafico dei tempi in funzione di n
+                system(("python3 graficoQualita.py " + std::to_string(k_fissato)).c_str());
+            }
+    }
+
+
 
 //----------------------------------------------CRONOMETRO----------------------------------------------
     // Fine del conteggio del tempo
